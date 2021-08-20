b2d2fefb81f84e300b1027331385f3c219a7a866
Myriad-Dreamin <camiyoru@gmail.com>
2021-08-20 20:28:44 +0800

    dev: delete useless testcase

--- src/drivers/mega/action.spec.ts
+++ src/drivers/mega/action.spec.ts
@@ -7,17 +7,6 @@ jest.setTimeout(5000000);
 describe('httpfs', function () {
   GenericUrlAction.registerByDomain('mega.nz', MegaUrlAction);
 
-  it('stat mega file', async () => {
-    const volume = await createAndLoadHttpVolume('https://mega.nz/file/6NUlDYpB#eAPUsx_wsDJ5tU8SYP94dkXHw1mGrDj5TBe0BfeL3bU');
-    expect(volume).toBeDefined();
-
-    const statRes = volume.statSync('/');
-    expect(statRes.isFile()).toBeTruthy();
-    const statByNameRes = volume.statSync('/(C84) [幽玄エレコード] 羡望ファーストノート.rar');
-    expect(statRes).toEqual(statByNameRes);
-  });
-
-
   const createMegaHttpFsVolume = async () => await createAndLoadHttpVolume('https://mega.nz/folder/mphTHa7Y#tDZzOlzLE7nNIMNT5K1Hag');
 
   it('statDir', HttpTestSuite.get(HttpTestSuite.statDir, createMegaHttpFsVolume));
